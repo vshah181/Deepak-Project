Add: GSCI dates Fix: Return calculation Optimise: Return calculation by reading year files as a dictionary

future_timeseries.py
@@ -2,11 +2,7 @@
 @author Varun Shah
 A program to find the next trading date given a date and optionally an RIC
 
-This module might need to be renamed to something better.
 """
-
-# Front contract means the next contract due to expire.
-
 
 import numpy as np
 import pandas as pd
@@ -14,19 +10,42 @@ import calendar
 import math
 import datetime
 
-MONTH_DICT = {'F': 'Jan',  # I keep forgetting these, which is why it's here.
-              'G': 'Feb',
-              'H': 'Mar',
-              'J': 'Apr',
-              'K': 'May',
-              'M': 'Jun',
-              'N': 'Jul',
-              'Q': 'Aug',
-              'U': 'Sep',
-              'V': 'Oct',
-              'X': 'Nov',
-              'Z': 'Dec'
+# TODO Make the user roll date customizable relative to gsci roll date
+#  (+/- n days)
+#  (Remember to check if the contract has expired - logger warning or error)
+
+# TODO Check that the rolling days aren't holidays.
+
+MONTH_DICT = {'F': 1,  # Jan (I keep forgetting these, which is why it's here.)
+              'G': 2,  # Feb
+              'H': 3,  # Mar
+              'J': 4,  # Apr
+              'K': 5,  # May
+              'M': 6,  # Jun
+              'N': 7,  # Jul
+              'Q': 8,  # Aug
+              'U': 9,  # Sep
+              'V': 10,  # Oct
+              'X': 11,  # Nov
+              'Z': 12  # Dec
               }
+
+prices_dict = {'2007': pd.read_csv('2007.csv'),
+               '2008': pd.read_csv('2008.csv'),
+               '2009': pd.read_csv('2009.csv'),
+               '2010': pd.read_csv('2010.csv'),
+               '2011': pd.read_csv('2011.csv'),
+               '2012': pd.read_csv('2012.csv'),
+               '2013': pd.read_csv('2013.csv'),
+               '2014': pd.read_csv('2014.csv'),
+               '2015': pd.read_csv('2015.csv'),
+               '2016': pd.read_csv('2016.csv'),
+               '2017': pd.read_csv('2017.csv'),
+               '2018': pd.read_csv('2018.csv'),
+               '2019': pd.read_csv('2019.csv'),
+               '2020': pd.read_csv('2020.csv'),
+               '2021': pd.read_csv('2021.csv')
+               }
 
 
 def go_to_last_business_day(some_date):
@@ -36,6 +55,21 @@ def go_to_last_business_day(some_date):
         return some_date - np.timedelta64(2, 'D')
     else:
         return some_date
+
+
+def get_eighth_business_day(string_date):
+    # Date is the first day of a given month
+    # 1st Mon - Wed becomes 10th
+    date = pd.to_datetime(string_date)
+    if 0 <= date.weekday() <= 2:
+        eighth_business_day = date + np.timedelta64(9, 'D')
+    # 1st Thurs - Sat becomes 12th
+    elif 3 <= date.weekday() <= 5:
+        eighth_business_day = date + np.timedelta64(11, 'D')
+    # 1st Sun becomes 11th
+    else:
+        eighth_business_day = date + np.timedelta64(10, 'D')
+    return eighth_business_day.strftime('%d/%m/%Y')
 
 
 def add_months(user_date, months_to_add):
@@ -57,7 +91,8 @@ def add_months(user_date, months_to_add):
 
 
 class ContinuousTimeseries:
-    def __init__(self, start_date, ric, k_contracts=1, n_months=False):
+    def __init__(self, start_date, ric, k_contracts=1, n_months=False,
+                 use_gsci=False):
         # Constructor method
         self.start_date = pd.to_datetime(start_date)
 
@@ -76,16 +111,52 @@ class ContinuousTimeseries:
             = go_to_last_business_day(pd.to_datetime(datetime.datetime.now().
                                                      strftime('%Y-%m-%d')))
         self.full_df = pd.read_csv('metaMaster.csv')
+        self.use_gsci = use_gsci
+
+    def add_gsci_column(self):
+        gsci_info = pd.read_csv('gsci.csv', index_col=0)
+        gsci_column = [np.nan] * len(self.full_df['ticker'])
+        for (cmdty, data) in gsci_info.iteritems():
+            for ticker in self.full_df['ticker']:
+                df_index = self.full_df['ticker'].where(
+                    self.full_df['ticker'] == ticker).notna().idxmax()
+                if ticker[:2] == cmdty:
+                    cmdty_series = gsci_info[ticker[:2]]
+                    cmdty_month_series = cmdty_series.where(cmdty_series
+                                                            == ticker[3])
+                    if (cmdty_month_series.notna()[::-1].idxmax() == 12
+                            and cmdty_month_series.notna().idxmax() == 1):
+                        # Then we need to iterate from the beginning
+                        month = 1
+                        while True:
+                            if cmdty_month_series.notna()[month + 1]:
+                                month += 1
+                            else:
+                                break
+                    else:
+                        month = cmdty_month_series.notna()[::-1].idxmax()
+
+                    if MONTH_DICT[ticker[2]] > month:
+                        # Then we need to go to the next year
+                        year = int(ticker[3:5]) + 2001
+                    else:
+                        # Then we stay in the same year
+                        year = int(ticker[3:5]) + 2000
+                    date_string = str(month) + '/' + str(year)
+                    gsci_roll_date = get_eighth_business_day(date_string)
+                    gsci_column[df_index] = gsci_roll_date
+                else:
+                    gsci_column[df_index] = np.nan
+        self.full_df["GSCIRollDT"] = gsci_column
 
     def get_kth_contract(self, date, given_ric, contract_k):
-        """
-        Finds the next trading date
-        :param given_ric:
-        :param date:
-        :return: next_date[pd_datetime]
-        """
+        if self.use_gsci:
+            self.add_gsci_column()
+            date_column = 'GSCIRollDT'
+        else:
+            date_column = 'myRollDT'
         working_df = self.get_ric_contracts(given_ric)
-        roll_series = pd.to_datetime(working_df['myRollDT'], dayfirst=True)
+        roll_series = pd.to_datetime(working_df[date_column], dayfirst=True)
         date_df = pd.DataFrame(
             dict(roll_date=roll_series, given_date=date))
         date_df['diff_days'] = (date_df['roll_date'] - date_df['given_date'])
@@ -100,43 +171,38 @@ class ContinuousTimeseries:
         return next_date.index
 
     def get_ric_contracts(self, given_ric):
-        """
-        If an RIC is specified then makes a working dataframe containing only
-        the specified RIC.
-        :return: ric_df; the working dataframe.
-        """
         ric_df = self.full_df.where(self.full_df['code'] == given_ric).dropna()
         return ric_df
 
-    def append_prices(self, given_timeseries):
-        price_array = []
+    def append_prices_and_returns(self, given_timeseries):
+        price_list = []
+        return_list = []
+        first_price = True
         for j in range(0, len(given_timeseries)):
             ticker = given_timeseries['Ticker'][j]
             year = '20' + given_timeseries['Ticker'][j][3:5]
             date = pd.to_datetime(given_timeseries['Date'][j], dayfirst=True)
-            date = datetime.datetime.strftime(date, "%Y-%m-%d")
-            year_df = pd.read_csv(year + '.csv')
+            date_str = datetime.datetime.strftime(date, "%Y-%m-%d")
+            year_df = prices_dict[year]
             try:
-                price = year_df.loc[year_df['date'] == date][ticker].iloc[0]
+                price = year_df.loc[year_df['date']
+                                    == date_str][ticker].iloc[0]
+                price_index = year_df.loc[year_df['date']
+                                    == date_str][ticker].index[0]
+                if first_price:
+                    this_return = np.nan
+                    first_price = False
+                else:
+                    prices_til_now = year_df[ticker].dropna().loc[:price_index]
+                    last_day_price = prices_til_now.iloc[-2]
+                    this_return = (price / last_day_price) - 1
             except IndexError:
                 price = np.nan
-            price_array.append(price)
-        given_timeseries['Price'] = price_array
-        return given_timeseries
-
-    def append_return(self, given_timeseries):
-        return_array = [np.nan]
-        price_array = given_timeseries['Price'].to_numpy(dtype=float)
-        for j in range(1, len(price_array)):
-            indexer = j-1
-            if np.isnan(price_array[j]):
-                return_array.append(np.nan)
-            else:
-                while np.isnan(price_array[j] / price_array[indexer]):
-                    indexer -= 1
-                return_array.append(price_array[j] / price_array[indexer])
-
-        given_timeseries['Return'] = return_array
+                this_return = np.nan
+            price_list.append(price)
+            return_list.append(this_return)
+        given_timeseries['Price'] = price_list
+        given_timeseries['Return'] = return_list
         return given_timeseries
 
     def build_ric_timeseries(self, given_ric):
@@ -170,8 +236,7 @@ class ContinuousTimeseries:
                 timeseries_df = timeseries_df.append(timeseries_new_row,
                                                      ignore_index=True)
 
-        timeseries_df = self.append_prices(timeseries_df)
-        timeseries_df = self.append_return(timeseries_df)
+        timeseries_df = self.append_prices_and_returns(timeseries_df)
         return timeseries_df
 
     def build_timeseries(self):
@@ -180,6 +245,10 @@ class ContinuousTimeseries:
             timeseries_list.append(self.build_ric_timeseries(given_ric))
         return timeseries_list
 
+    def check_var(self):
+        self.add_gsci_column()
+        return self.full_df
+
 
 obj = ContinuousTimeseries("2019-01-01", ['w', 'c'])
-a = obj.build_timeseries()
+fdf = obj.check_var()